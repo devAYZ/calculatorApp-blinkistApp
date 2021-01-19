08d0cefbbeefd3f7ad92ea8f672452dbcbe8cc0b
ayokunle <ayokunle.fatokimi@decagon.dev>
2021-01-19 21:50:48 +0100

    add a segue from the home tab to the third button

---
+++ ASectionOfBlinkistApp/ASectionOfBlinkistApp/Controllers/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  ASectionOfBlinkistApp
+//
+//  Created by Ayokunle on 19/01/2021.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Book", for: indexPath) as? BookCell else{
+            fatalError("Unable to dequeue Book Cell")
+        }
+        
+        return cell
+    }
+    
+
+
+}
---
+++ ASectionOfBlinkistApp/ASectionOfBlinkistApp/View/BookCell.swift
@@ -0,0 +1,14 @@
+//
+//  BookCell.swift
+//  ASectionOfBlinkistApp
+//
+//  Created by Ayokunle on 19/01/2021.
+//
+
+import UIKit
+
+class BookCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
