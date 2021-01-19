e70c1c984aab8acad2a6052d890980e36f83e5cf
ayokunle <ayokunle.fatokimi@decagon.dev>
2021-01-19 02:23:25 +0100

    add first button to Calculator

---
+++ FunctionalCalculator/FunctionalCalculator/Model/RoundBorder.swift
@@ -0,0 +1,23 @@
+//
+//  RoundBorder.swift
+//  FunctionalCalculator
+//
+//  Created by Ayokunle on 19/01/2021.
+//
+
+import UIKit
+
+extension UIImageView{
+    func roundImg(){
+        self.layer.cornerRadius = self.frame.size.width / 2.0
+        self.clipsToBounds = true
+    }
+}
+
+
+extension UIButton{
+    func roundBtn(){
+        self.layer.cornerRadius = 13
+        self.clipsToBounds = true
+    }
+}
