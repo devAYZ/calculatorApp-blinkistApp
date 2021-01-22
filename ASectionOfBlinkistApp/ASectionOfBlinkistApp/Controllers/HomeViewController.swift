9394bda52ee47020691826b9486ade5fef987daa
ayokunle <ayokunle.fatokimi@decagon.dev>
2021-01-22 17:01:50 +0100

    add some cleaning up

--- ASectionOfBlinkistApp/ASectionOfBlinkistApp/Controllers/HomeViewController.swift
+++ ASectionOfBlinkistApp/ASectionOfBlinkistApp/Controllers/HomeViewController.swift
@@ -23,14 +23,6 @@ class HomeViewController: UICollectionViewController {
        typealias DataSource = UICollectionViewDiffableDataSource<Section, Book>
        typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Book>
 
-
-       // MARK: - Life Cycles
-       override func viewDidLoad() {
-         super.viewDidLoad()
-         configureLayout()
-         
-         applySnapshot(animatingDifferences: false)
-       }
        
        // MARK: - Functions
        func makeDataSource() -> DataSource {
@@ -64,29 +56,4 @@ class HomeViewController: UICollectionViewController {
 
 }
 
-     // MARK: - Layout Handling
-     extension HomeViewController {
-       private func configureLayout() {
-         collectionView.collectionViewLayout = UICollectionViewCompositionalLayout(sectionProvider: { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
-           let isPhone = layoutEnvironment.traitCollection.userInterfaceIdiom == UIUserInterfaceIdiom.phone
-           let size = NSCollectionLayoutSize(
-             widthDimension: NSCollectionLayoutDimension.fractionalWidth(1),
-             heightDimension: NSCollectionLayoutDimension.absolute(isPhone ? 280 : 250)
-           )
-           let itemCount = isPhone ? 1 : 3
-           let item = NSCollectionLayoutItem(layoutSize: size)
-           let group = NSCollectionLayoutGroup.horizontal(layoutSize: size, subitem: item, count: itemCount)
-           let section = NSCollectionLayoutSection(group: group)
-           section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
-           section.interGroupSpacing = 10
-           return section
-         })
-       }
-       
-       override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-         super.viewWillTransition(to: size, with: coordinator)
-         coordinator.animate(alongsideTransition: { context in
-           self.collectionView.collectionViewLayout.invalidateLayout()
-         }, completion: nil)
-       }
-     }
+    
